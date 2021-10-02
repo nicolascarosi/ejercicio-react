a62dbe208c45fa3e488539b3becd017e57271a21
Nicolás Carosi <carosinicolas@gmail.com>
2021-10-02 00:55:19 -0300

    Updates

--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-export const CurrencyFormat = (value, decimals=2) => {
-    if (!value) return '$ 0';
-    const val = parseFloat(value);
-    return '$ ' + val.toFixed(decimals).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-}
